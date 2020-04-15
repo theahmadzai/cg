d12321d9fd33352624d7fb306c107c0707fd3d95
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-04-15 23:38:28 +0500

    Updated

--- src/graphics/line/DDA.java
+++ src/graphics/line/DDA.java
@@ -1,7 +1,16 @@
 package graphics.line;
 
+import java.awt.Point;
+
 public class DDA {
-	public DDA() {
-		
+	public DDA(Point p, Point q) {
+		double dx = p.getX() / q.getX();
+		double dy = p.getY() / q.getY();
+
+		double steps = Math.max(dx, dy);
+
+		dx = dx / steps;
+		dy = dy / steps;
+
 	}
 }
